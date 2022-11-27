f59db1cc6dad877bf80ddd5c44a0554e0d5e9d94
Zak Farmer <zak@3sidedcube.com>
2022-11-27 14:31:32 +0000

    Fixed lib ICE due to no_std being passed

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(rs_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    serial_println!("PANIC: {}", _info);
-    loop {}
-}
+use rs_os::println;
 
 // Define entry point
 #[no_mangle]
@@ -23,21 +19,17 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
-#[cfg(test)]
-fn test_runner(tests: &[&dyn test::Testable]) {
-    use crate::qemu::{exit_qemu, QemuExitCode};
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+ 
 
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-
-    // Finished running tests, close QEMU
-    exit_qemu(QemuExitCode::Success);
+    println!("{}", info);
+    loop {}
 }
 
-#[test_case]
-fn test_println_simple() {
-    println!("test_println_simple output");
-    assert_eq!(1, 1);
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rs_os::test_panic_handler(info)
 }
